1f0d22a178e00066894b38e6a3609d9e13009798
codasirrah <harristurner97@gmail.com>
2020-11-05 19:06:27 +0000

    Added a css file. Changed the Javascript logic to work with the DOM so it can be presented inside the webpage rather than on the console.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-An algorithm to allow the user to play the classic game Rock, Paper, Scissors!
+Some code to allow the user to play the classic game Rock, Paper, Scissors in the web browser!
--- rps.js
+++ rps.js
@@ -1,3 +1,24 @@
+const resultsContainer = document.querySelector("#resultsContainer")
+const scoreContainer = document.querySelector("section");
+let playerChoice = document.createElement("p");
+let pcChoice = document.createElement("p");
+let playerScore = document.createElement("p");
+let pcScore = document.createElement("p");
+let score;
+let intScorePlayer = 0;;
+let intScorePc = 0;
+const announceContainer = document.querySelector("#announceContainer");
+let message = document.createElement("h3");
+let score1C = document.querySelector("#score1C");
+let score2C = document.querySelector("#score2C")
+let reset = document.querySelector(".reset");
+
+
+playerScore.classList.add("sectionPlayer"); 
+pcScore.classList.add("sectionPc");
+
+
+
 // creates function that returns either rock, paper or scissors as computer input.
 function computerPlay() {
     let x = Math.floor(Math.random()*3);
@@ -9,61 +30,157 @@ function computerPlay() {
         computerSelection = "Scissors";
     }
 }
-let score = 0;
+
 // creates function that plays a round measuring the playerSelection against the computerSelection to dermine the outcome message.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock") {
         if (computerSelection === "Rock") {
-        return message = console.log("You drew!");
+        return score = "draw";
         } else if (computerSelection === "Paper") {
-            score -= 1;
-            return message = console.log("You lost! Paper beats rock.");
+            return score = "loser"
         } else {
-            score += 1;
-            return message = console.log("You won! Rock beats scissors.");
+            return score = "winner"
     }   
 }
     if (playerSelection === "paper") {
         if (computerSelection === "Paper") {
-            return message = console.log("You drew!");
+            return score = "draw";
         } else if (computerSelection === "Scissors") {
-            score -= 1;
-            return message = console.log("You lost! Scissors beats Paper.");
+            return score = "loser"
         } else {
-            score += 1;
-            return message = console.log("You won! Paper beats rock!");
+            return score = "winner";
         }
     }
     if (playerSelection === "scissors") {
         if (computerSelection === "Scissors") {
-            return message = console.log("You drew!");
+            return score = "draw";
         } else if (computerSelection === "Rock") {
-            score -= 1;
-            return message = console.log("You lost! Rock beats scissors.");
+            return score = "loser";
         } else {
-            score += 1;
-            return message = console.log("You won! Scissors beats paper.");
+            return score = "winner";
         }
     }
 }
-
-// creates a function that loops calls the playRound function 5 times and tallies the total score.
-function game() {
-let i;
-    for (i = 0; i < 5; i++) {
-        computerPlay();
-        playRound(prompt("Rock, Paper or Scissors?:").toLowerCase(), computerSelection);
-        console.log("Total Score: " + score);
-    }
+function removeTransform(e) {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("select");
 }
-// call the game function.
-game();
 
-// prints the final outcome of the match. Did you win, lose or draw?
-if (score === 0) {
-    console.log("In a best of five you are equals.");
-} else if(score < 0) {
-    console.log("The computer has beaten you!");
-} else {
-    console.log("Congratulations! You have bested the computer.")
+function selectRock(e) {
+    e.classList.add("select");
+    playerChoice.textContent = "You picked rock!";
+    resultsContainer.appendChild(playerChoice);
+    computerPlay();
+    pcChoice.textContent = `The computer picked ${computerSelection}!`;
+    resultsContainer.appendChild(pcChoice);
+    playRound("rock", computerSelection);
 }
+
+function selectPaper(e) {
+    e.classList.add("select");
+    playerChoice.textContent = "You picked paper!";
+    resultsContainer.appendChild(playerChoice);
+    computerPlay();
+    pcChoice.textContent = `The computer picked ${computerSelection}!`;
+    resultsContainer.appendChild(pcChoice);
+    playRound("paper", computerSelection);
+}
+
+function selectScissors(e) {
+    e.classList.add("select");
+    playerChoice.textContent = "You picked scissors!";
+    resultsContainer.appendChild(playerChoice);
+    computerPlay();
+    pcChoice.textContent = `The computer picked ${computerSelection}!`;
+    resultsContainer.appendChild(pcChoice);
+    playRound("scissors", computerSelection);
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (button.classList.contains("rock")) { 
+            selectRock(button);
+        button.addEventListener("transitionend", removeTransform);
+        if (score === "winner")  {
+            intScorePlayer += 1;
+            playerScore.innerHTML = `You: ${intScorePlayer}`;
+            score1C.appendChild(playerScore);
+            if (intScorePlayer === 5) {
+                message.innerText = `Congratualtions, you have won with a score of ${intScorePlayer} : ${intScorePc} \nGood job!`;
+                message.classList.add("announcement");
+                announceContainer.appendChild(message);
+            }
+            }
+        if (score === "loser") {
+            intScorePc += 1;
+            pcScore.innerHTML = `Computer: ${intScorePc}`;
+            score2C.appendChild(pcScore);
+            if (intScorePc === 5) {
+                message.innerText = `Ouch! You have lost with a score of ${intScorePlayer} : ${intScorePc} \nTry again!`;
+                message.classList.add("announcement");
+                announceContainer.appendChild(message);
+            }
+            }
+        }
+        if (button.classList.contains("paper")) {
+            selectPaper(button);
+            button.addEventListener("transitionend", removeTransform);
+            if (score === "winner")  {
+                intScorePlayer += 1;
+                playerScore.innerHTML = `You: ${intScorePlayer}`;
+                score1C.appendChild(playerScore);
+                if (intScorePlayer === 5) {
+                    message.innerText = `Congratualtions, you have won with a score of ${intScorePlayer} : ${intScorePc} \nGood job!`;
+                    message.classList.add("announcement");
+                    announceContainer.appendChild(message);
+                }
+                }
+                if (score === "loser") {
+                    intScorePc +=1;
+                    pcScore.innerHTML = `Computer: ${intScorePc}`;
+                    score2C.appendChild(pcScore);
+                    if (intScorePc === 5) {
+                        message.innerText = `Ouch! You have lost with a score of ${intScorePlayer} : ${intScorePc} \nTry again!`;
+                        message.classList.add("announcement");
+                        announceContainer.appendChild(message);
+                    }
+                    }
+             }
+         if (button.classList.contains("scissors")) {
+            selectScissors(button);
+            button.addEventListener("transitionend", removeTransform);
+            if (score === "winner")  {
+            intScorePlayer += 1;
+            playerScore.innerHTML = `You: ${intScorePlayer}`;
+            score1C.appendChild(playerScore);
+            if (intScorePlayer === 5) {
+                message.innerText = `Congratualtions, you have won with a score of ${intScorePlayer} : ${intScorePc} \nGood job!`;
+                message.classList.add("announcement");
+                announceContainer.appendChild(message);
+            }
+            }
+            if (score === "loser") {
+                intScorePc +=1;
+                pcScore.innerHTML = `Computer: ${intScorePc}`;
+                score2C.appendChild(pcScore);
+                if (intScorePc === 5) {
+                    message.innerText = `Ouch! You have lost with a score of ${intScorePlayer} : ${intScorePc} \nTry again!`;
+                    message.classList.add("announcement");
+                    announceContainer.appendChild(message);
+                }
+                }
+            }
+        
+    });
+});
+
+reset.addEventListener("click", (r) => {
+    intScorePc = 0, intScorePlayer = 0;
+    pcScore.remove(), playerScore.remove();
+    message.remove();
+    pcChoice.remove(), playerChoice.remove();
+});
+
+
+
